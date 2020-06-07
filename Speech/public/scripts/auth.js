4f359013781ee49b3561575be1777f3bb277d812
Bearcat202020 <jbarakat20@kentdenver.org>
2020-06-07 13:02:54 -0600

    added authentication for signup/login/logout, added read functionality for logged in users

--- Speech/public/scripts/auth.js
+++ Speech/public/scripts/auth.js
@@ -1,17 +1,94 @@
-
 const auth = firebase.auth()
+const db = firebase.firestore()
 
+
+
+
+
+//signing up
 const signupForm = $("#signup-form")[0];
 signupForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const email = $("#signup-email")[0].value;
   const password = $("#signup-password")[0].value;
+  const name = $("#signup-name")[0].value;
 
-  console.log(email);
   auth.createUserWithEmailAndPassword(email, password).then(cred => {
+
+    db.collection("users").doc(cred.user.uid).set({
+      name: name
+    })
+
+    // db.collection("users").doc(cred.user.uid).collection("practices").doc("Practice 0").set({
+    //   feedback: "View Your Practices Here"
+    // });
+
     const modal = $("#modal-signup")[0];
     M.Modal.getInstance(modal).close();
     signupForm.reset();
   });
 
 });
+
+
+//sign in
+const loginForm = $("#login-form")[0];
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const email = $("#login-email")[0].value;
+  const password = $("#login-password")[0].value;
+
+  auth.signInWithEmailAndPassword(email, password).then(cred => {
+    console.log(cred)
+    const modal = $("#modal-login")[0];
+    M.Modal.getInstance(modal).close();
+    loginForm.reset();
+  });
+
+
+
+})
+
+
+//logging out
+const logout = $("#logout")[0];
+logout.addEventListener('click', (e) => {
+  e.preventDefault();
+  auth.signOut().then(cred => {
+  });
+});
+
+
+
+auth.onAuthStateChanged(user => {
+  if(user){
+
+    //GRABS ALL DATA UNDERNEATH YOUR NAME
+    db.collection("users").doc(user.uid).collection("Practices").get().then(qSnap => {
+
+      let html = ""
+      qSnap.forEach( doc =>  {
+        const practice = doc.data()
+        const coach = practice.Coach;
+        const feedback = practice.Feedback;
+
+        html += "<li><div class='collapsible-header grey lighten-4'>Practice with: " + coach + "</div><div class='collapsible-body white'><span>" + feedback + "</span></div>"
+
+      });
+
+      $(".guides").html(html);
+    });
+  }
+  else{
+    $(".guides").html("<li><div class='collapsible-header grey lighten-4'>Please Login/SignUp to see practices</div><div class='collapsible-body white'><span>Lorem ipsum dolor sit amet.</span></div>")
+
+  }
+
+})
+//
+// const myPractice = db.collection("users").doc(cred.user.uid).collection("practices");
+// myPractice.get().then(snap => {
+//
+//   console.log(snap.docs)
+// })
